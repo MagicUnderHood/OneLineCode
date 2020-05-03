76dbbd6a1db0981c15ef3a7c2b4d0ad6413b08f9
Tanya <tanya.orlova17@gmail.com>
2020-05-03 18:31:45 +0300

    Added fibonacci calculation with fold() and test

--- src/FibonacciSeries.kt
+++ src/FibonacciSeries.kt
@@ -1,5 +1,4 @@
 fun main(){
-    val foo = (0..5).map { fibonacci(it) }
-    println(foo)
+    println((0..12).map { fibonacci(it) })
 }
 fun fibonacci(n: Int): Int =  if (n == 0) 0 else if (n == 1) 1 else fibonacci(n - 1) + fibonacci(n - 2)
